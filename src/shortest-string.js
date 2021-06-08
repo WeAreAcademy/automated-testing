87eb48feceed2848a92017cf11c13bafff7d1c45
JWorthington97 <jb.worthington@live.co.uk>
2021-06-08 11:45:46 +0100

    Recommit - Merge to wrong repo

--- src/shortest-string.js
+++ src/shortest-string.js
@@ -17,6 +17,7 @@ function findShortestString(stringArr) {
   return shortestString;
 }
 
+
 // console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
 // console.log(findShortestString(["why", "hello", "to", "you"]), "to");
 // console.log(findShortestString(["brave", "dance"]), "brave");
