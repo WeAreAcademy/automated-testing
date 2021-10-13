479070a5b237d97f1b32a0f3ea6ef2f649c75813
Grace Zaborski <gracezaborski@Graces-Beamery-MacBook-Pro.local>
2021-10-13 13:58:42 +0100

    Shortest_string_file

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -1,12 +1,12 @@
 // import function from local file
-const findLongestString = require("./find-longest-string");
+const findLongestString = require("./find-shortest-string");
 
-test("findLongestString finds the longest string in an array", () => {
+test("findShortestString finds the shortest string in an array", () => {
   expect(findLongestString(["it", "is", "a", "nice", "day"])).toBe("nice");
-  expect(findLongestString(["why", "hello", "to", "you"])).toBe("hello");
+  expect(findLongestString(["why", "hello", "to", "you"])).toBe("to");
 });
 
-test("findLongestString returns the earlier string in cases of joint longest strings", () => {
+test("findShortestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
   expect(findLongestString(["hello", "goodb"])).toBe("hello");
   expect(findLongestString(["a", "b", "c"])).toBe("a");
